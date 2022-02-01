618af136fc993cab87be30a38ab74f45499a2958
Endale Teggnework <endalework90@gmail.com>
2022-02-02 00:30:45 +0300

    mounting the routers for separation

--- app.js
+++ app.js
@@ -101,6 +101,41 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This file is not defined yet',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This file is not defined yet',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This file is not defined yet',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This file is not defined yet',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This file is not defined yet',
+  });
+};
+
 // app.get('/api/v1/tours', getTours);
 // app.post('/api/v1/tours', createTour);
 // app.get('/api/v1/tours/:id', getTour);
@@ -108,12 +143,17 @@ const deleteTour = (req, res) => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 // 3) ROUTING
-app.route('/api/v1/tours').get(getTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+tourRouter.route('/').get(getTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 //4) STARTING SERVER
 const port = 3000;
