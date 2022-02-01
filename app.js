e1cad5b02a41169ee0376ec460a1619507b595b4
Endale <82932616+endx-star@users.noreply.github.com>
2022-02-01 21:19:54 +0300

    getting all tours in the file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const fs     = require('fs');
 
 const app = express();
 
-app.get('/', () => {
-  console.log('hello from the server');
-});
+app.get('/api/v1/tours', (req, res) => {
+ 
+})
 const port = 3000;
 app.listen(3000, () => {
   console.log(`The server is runing on port ${port}....`);
